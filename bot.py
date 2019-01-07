a9b8e870e68e8d772a4c7aa6ea2f37e505c62a8f
kurg <kvrensburg@gmail.com>
2019-01-06 22:39:49 -0500

    Initial commit

---
+++ bot.py
@@ -0,0 +1,7 @@
+print("Hi, I am Kendy, your personal bot.")
+UserName = input("Please enter your name : ")
+print("Hello, " + UserName + ", nice to meet you.")
+number1 = input("Number 1 > :")
+number2 = input("Number 2 > :")
+result = number1 + number2
+print(result)
---
+++ helloworld.py
@@ -0,0 +1,6 @@
+print("Hi!")
+print("I am a computer program")
+my_name = "PyProg"
+print("My name is " + my_name)
+users_name = input("What is your name? ")
+print("Hello " + users_name + ", welcome!")
