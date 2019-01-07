80a462b8e05749e681420948b01e7faa8e47999a
kurg <kvrensburg@gmail.com>
2019-01-06 22:58:58 -0500

    Update bot.py
    updated changes

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 print("Hi, I am PyProg, your personal bot.")
 UserName = input("Please enter your name : ")
 print("Hello, " + UserName + ", nice to meet you.")
-number1 = input("Number 1 > :")
-number2 = input("Number 2 > :")
-result = number1 + number2
+input1 = input("Number 1 > :")
+input2 = input("Number 2 > :")
+result = input1 + input2
 print(result)
