c4a765e8d782903baffbe570b51431a25e4b5a87
kurg <kvrensburg@gmail.com>
2019-01-06 22:46:47 -0500

    Update bot.py
    changed Kendy to PyProg

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-print("Hi, I am Kendy, your personal bot.")
+print("Hi, I am PyProg, your personal bot.")
 UserName = input("Please enter your name : ")
 print("Hello, " + UserName + ", nice to meet you.")
 number1 = input("Number 1 > :")
